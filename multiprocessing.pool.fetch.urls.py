44fe17c48278ea45b7db8c7dd1606cf0cc747296
Khalid Zubair <37196505+thefattman@users.noreply.github.com>
2018-04-11 05:45:01 +0200

    updated wrong file - correcting

--- multiprocessing.pool.fetch.urls.py
+++ multiprocessing.pool.fetch.urls.py
@@ -1,36 +1,31 @@
 import requests 
-import threading 
 from time import time
+import multiprocessing 
 from pprint import pprint
 
 
-def fetch_file(index, results): 
+def fetch_file(index): 
     print(F"Thread #{index + 1}")
     start = time()
     r = requests.get("http://speedtest.ftp.otenet.gr/files/test1Mb.db")
-    results[index] = {
-        'size': len(r.text), 
-        'time': time() - start 
-    }
     print(F"done #{index + 1}")
+    return dict(size=len(r.text), time=time()-start), index
     
 if __name__ == '__main__': 
     start = time()
-    
-    results = {}
-    for i in range(20): 
-        t = threading.Thread(target=fetch_file, args=(i, results))
-        t.start()
+    results  = {}
+    with multiprocessing.Pool(4) as pool:
+        threads = [
+            pool.apply_async(fetch_file, (i,))
+            for i in range(20)
+        ]
         
-    main_thread = threading.main_thread()
-    
-    for thread in threading.enumerate(): 
-        if thread is main_thread: 
-            continue 
+        for t in threads: 
+            result, index = t.get()
+            results[index] = result
             
-        thread.join()
         
-    
+
     
     print(F"All done {time() - start}(s)")
     pprint(results)
